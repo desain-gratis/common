cedca78ae11791cfb61ef1a4c1facddb67d2888c
keenan.gebze <keenan.gebze@gmail.com>
2026-09-20 03:34:04 +0700

    fix url handling in mycontent get post-process

--- delivery/mycontent-api/postprocess.go
+++ delivery/mycontent-api/postprocess.go
@@ -12,17 +12,20 @@ type URLFormat func(baseURL string, userID string, refIDs []string, ID string) s
 // FormatURL inplace
 func FormatURL[T mycontent.Data](baseURL string, params []string) func(t T) {
 	return func(t T) {
-		refIDs := t.RefIDs()
-		if refIDs == nil || len(refIDs) != len(params) {
+		u, err := url.Parse(baseURL)
+		if err != nil {
 			return
 		}
 
-		u, _ := url.Parse(baseURL)
 		q := make(url.Values)
-		for idx, param := range params {
-			q[param] = []string{refIDs[idx]}
-		}
 		q["id"] = []string{t.ID()}
+
+		refIDs := t.RefIDs()
+		if refIDs != nil && len(refIDs) == len(params) {
+			for idx, param := range params {
+				q[param] = []string{refIDs[idx]}
+			}
+		}
 
 		u.RawQuery = q.Encode()
 		t.WithURL(u.String())
